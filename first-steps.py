327b30d7f519f5695eb3948b91acbe326a7fb108
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-31 06:02:29 -0400

    python | 2

--- first-steps.py
+++ first-steps.py
@@ -2,3 +2,17 @@ array = "texto"
 
 print(type(array)) #<class 'str'>
 print(len(array))  # 5 <--- len nos dice el lenght
+
+print(array[0])
+print(array[1])
+print(array[2])
+print(array[3])
+print(array[4])
+
+""" si llamamos array[5] nos salta un error, cuidado con  """
+
+rebanada = array[1:4]
+print(rebanada) # ext
+
+num = input("Ingrese un numero: ")
+print(num)
