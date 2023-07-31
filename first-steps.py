7e829b4072873b78568a66cc455f3d9053c67d08
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-31 05:24:08 -0400

    python | length

---
+++ first-steps.py
@@ -0,0 +1,4 @@
+array = "texto"
+
+print(type(array)) #<class 'str'>
+print(len(array))  # 5 <--- len nos dice el lenght
--- freeCodeCamp.py
+++ freeCodeCamp.py
@@ -9,3 +9,19 @@ y = 5
 
 print(x + y) #10
 print(x * y) #25
+
+result = x + y
+
+print(type(result)) # <class 'int'> ---> int means integer = entero
+
+z = 0.09
+
+print(type(z)) # <class 'float'> ---> float means flotante.
+
+""" boolean requiere mayuscula al principio
+True 
+False
+"""
+
+print(type(True))  #<class 'bool'>
+print(type(False)) #<class 'bool'>
