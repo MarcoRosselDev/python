f1076c4f4dba034029ae31cd7b8acfeb8cc64fdf
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-31 23:54:17 -0400

    python | diccionarios

--- insert.py
+++ insert.py
@@ -4,4 +4,15 @@ print(nums)
 
 numsAppend = [2,4,2]
 numsAppend.append(9) #append agrega al final
-print(numsAppend)
+print(numsAppend) 
+
+# diccionarios
+
+edades = {"gino":15, "nora": 45}
+
+if "gino" in edades:
+    print(True)
+
+edades['marco'] = 30
+
+print(edades)
