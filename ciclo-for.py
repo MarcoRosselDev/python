e570c057c31eb197935925823b40f88687f52f9d
Marco Rossel <andresmarcorossel@gmail.com>
2023-08-01 00:15:10 -0400

    python | for

---
+++ ciclo-for.py
@@ -0,0 +1,26 @@
+""" ciclos for
+
+Estructura de control en programacion que
+permite ejecutar una o varias lineas de codigo 
+multiples veces.
+
+los usamos cuando sabemos con antelacion cuantas
+veces debemos repetir ciertas intstrucciones.
+"""
+
+for i in range(4):
+    print(i)
+
+""" range(start, stop, [step]) 
+
+start
+    el valor del parametro start (0 si no se utiliza el parametro)
+    desde donde quiero que empieze a iterar.
+stop
+    el valor del parametro stop
+    para que se detenga en este n de iteracion.
+step
+    el valor del parametro step (1 si no se utiliza el parametro)
+    por defecto es 1, pero podemos hacer que se apliquen mas saltos entre cada iteracion.
+
+"""
