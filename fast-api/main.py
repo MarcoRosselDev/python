4c2f403965fef6616d94741356c2eb3c4df3087c
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-08-04 05:57:04 -0400

    python for backend | firs API | get users 2

--- fast-api/main.py
+++ fast-api/main.py
@@ -4,18 +4,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-class User():
-    name: str
-    lastName: str
-    age: int
-    url: str 
-
-users_list = [
-    User(name="marco", lastName="rossel",age=30 ,url="www.mrossel.com"),
-    User(name="eduardo", lastName="rossel",age=32 ,url="www.erossel.com"),
-    User(name="juan", lastName="medina",age=35 ,url="www.jmedina.com"),
-    User(name="jayson", lastName="ruminot",age=14 ,url="www.jsonRuminot.com"),
-]
 
 @app.get("/")
 async def read_root():
@@ -28,7 +16,7 @@ async def url():
 
 @app.get("/users")
 async def getUsers():
-    return { users_list }
+    return { "futuros usuarios"}
 # @app.get("/items/{item_id}")
 # def read_item(item_id: int, q: Union[str, None] = None):
 #     return {"item_id": item_id, "q": q}
