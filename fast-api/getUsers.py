a903023b7c84c4421e05d209c3402a36dcea9d73
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-08-04 06:13:06 -0400

    python for backend | firs API | get users 3

--- fast-api/getUsers.py
+++ fast-api/getUsers.py
@@ -1,22 +1,30 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 # Iniciar el server: uvicorn getUsers:app --reload
 
 @app.get("/users")
-async def users():
-    return "te enviaremos los usuarios proximamente"
+async def getUsers():
+    return [
+        {"name": "marco",
+        "lastName": "rossel",
+        "age": 30},
+        {"name": "Eduardo",
+        "lastName": "rossel",
+        "age": 32}
+    ]    
 
-class User():
-    name: str
-    lastName: str
-    age: int
-    url: str 
+# class User():
+#     name: str
+#     lastName: str
+#     age: int
+#     url: str 
 
-users_list = [
-    User(name="marco", lastName="rossel",age=30 ,url="www.mrossel.com"),
-    User(name="eduardo", lastName="rossel",age=32 ,url="www.erossel.com"),
-    User(name="juan", lastName="medina",age=35 ,url="www.jmedina.com"),
-    User(name="jayson", lastName="ruminot",age=14 ,url="www.jsonRuminot.com"),
-]
+# users_list = [
+#     User(name="marco", lastName="rossel",age=30 ,url="www.mrossel.com"),
+#     User(name="eduardo", lastName="rossel",age=32 ,url="www.erossel.com"),
+#     User(name="juan", lastName="medina",age=35 ,url="www.jmedina.com"),
+#     User(name="jayson", lastName="ruminot",age=14 ,url="www.jsonRuminot.com"),
+# ]
--- fast-api/type-hints.py
+++
@@ -1,7 +0,0 @@
-my_variable:str = "my string"
-print(type(my_variable)) # class str
-print(my_variable)
-
-my_variable = 5
-print(type(my_variable)) # class str
-print(my_variable)
