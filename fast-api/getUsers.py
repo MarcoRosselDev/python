1c3d84f360f9da651324e7c5a3bc58b9e9abad44
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-08-04 06:19:46 -0400

    python for backend | firs API | url users Class BaseModel

--- fast-api/getUsers.py
+++ fast-api/getUsers.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 # Iniciar el server: uvicorn getUsers:app --reload
 
-@app.get("/users")
+@app.get("/usersJSON")
 async def getUsers():
     return [
         {"name": "marco",
@@ -14,17 +14,21 @@ async def getUsers():
         {"name": "Eduardo",
         "lastName": "rossel",
         "age": 32}
-    ]    
+    ]
 
-# class User():
-#     name: str
-#     lastName: str
-#     age: int
-#     url: str 
 
-# users_list = [
-#     User(name="marco", lastName="rossel",age=30 ,url="www.mrossel.com"),
-#     User(name="eduardo", lastName="rossel",age=32 ,url="www.erossel.com"),
-#     User(name="juan", lastName="medina",age=35 ,url="www.jmedina.com"),
-#     User(name="jayson", lastName="ruminot",age=14 ,url="www.jsonRuminot.com"),
-# ]
+class User(BaseModel):
+    name: str
+    lastName: str
+    age: int
+    url: str 
+
+users_list = [
+    User(name="marco", lastName="rossel",age=30 ,url="www.mrossel.com"),
+    User(name="eduardo", lastName="rossel",age=32 ,url="www.erossel.com"),
+    User(name="juan", lastName="medina",age=35 ,url="www.jmedina.com"),
+    User(name="jayson", lastName="ruminot",age=14 ,url="www.jsonRuminot.com"),
+]
+@app.get("/userFromClass")
+async def getUsersBaseModal():
+    return users_list
