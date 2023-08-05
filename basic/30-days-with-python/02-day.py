feaa1a07e033d632265f994a5fa7cc376de059b8
Marco Rossel <andresmarcorossel@gmail.com>
2023-08-05 12:40:43 -0400

    python | backend | 02

---
+++ basic/30-days-with-python/02-day.py
@@ -0,0 +1,34 @@
+# day 2: 30 Days of python programming
+
+first_name = 'Marco'
+last_name = 'Rossel'
+full_name = first_name + ' ' + last_name
+
+print(full_name) # Marco Rossel
+print(type(full_name)) # class str
+
+country = 'Chilean'
+city = 'Concepcion'
+age = 30
+year = 2023
+is_married = False
+is_true = True
+is_light_on = False
+#multiple_variable
+a, b, c, d = 1, 2, 3, 4
+print(a, b, c, d) # 1, 2, 3, 4,
+
+# 5. The radium of a circle is 30 meters.
+# I. Calculate the area of a circle and assign the value to a variable name of area_of_circle
+
+radium = 30
+pi = 3.14
+area_of_circle = radium* pi**2
+
+print(area_of_circle) # 295.788 meters squer
+
+# II Calculate the circumference of a circle and assign the value to a variable name of circum_of_circle
+
+circum_of_circle = radium * 2 * pi
+
+print(circum_of_circle) # 188.4
