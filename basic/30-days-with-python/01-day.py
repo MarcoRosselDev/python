dbeb6104a141a58b08dfae821f08bc99b32bfb96
Marco Rossel <andresmarcorossel@gmail.com>
2023-08-05 11:34:30 -0400

    python | backend | 01

---
+++ basic/30-days-with-python/01-day.py
@@ -0,0 +1,43 @@
+# 1 how to check the python version ----> (in terminal) python --version
+
+# 2 Open th epython interactive shell and do the following operations. The operands are 3 and 4
+
+# addition (+)
+print("addition")
+print(3 + 4) # 7
+
+# subtraction (-)
+print("subtracion")
+print(3 -4) # -1
+
+# multiplication (*)
+print("multiplication")
+print(3 * 4) # 12
+
+# modulus (%)
+print("modulus")
+print(3 % 4) # 3
+
+# division (/)
+print("division")
+print(3 / 4) # 0.75
+
+# exponential (**)
+print("division")
+print(3 ** 4) # 81
+
+# floor division operator(//)
+print("floor division operator")
+print(3 // 4) # 0
+
+
+# 4 check the data types of the following data:
+
+print(type(10)) # class 'int'
+print(type(9.8)) # class 'float'
+print(type(3.14)) # class 'float'
+print(type(4 - 4j)) # class 'complex'
+print(type(['Asabeneh', 'Python', 'Finland'])) # class 'list'
+print(type('Marco')) # class 'str'
+print(type('Rossel')) # class 'str'
+print(type('Chilean')) # class 'str'
