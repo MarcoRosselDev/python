08edd035422c4835fa782fd808b8cbc2b39790db
Marco Rossel <andresmarcorossel@gmail.com>
2023-08-06 01:24:13 -0400

    python | backend | 03 - 1-5

--- basic/30-days-with-python/03_booleans.py
+++ basic/30-days-with-python/03_booleans.py
@@ -15,3 +15,32 @@ print('operador or : 1 < 5 or 1  > 10 = ', 1 < 5 or 1 > 10) # true
 
 # not -- reverse the result, return false if the result is ture
 print('operador not :  not(2 < 5) = ', not(2 < 5)) # false
+
+
+print(3 > 2 and 4 > 3) # True - because both statements are true
+print(3 > 2 and 4 < 3) # False - because the second statement is false
+print(3 < 2 and 4 < 3) # False - because both statements are false
+print('True and True: ', True and True)
+print(3 > 2 or 4 > 3)  # True - because both statements are true
+print(3 > 2 or 4 < 3)  # True - because one of the statements is true
+print(3 < 2 or 4 < 3)  # False - because both statements are false
+print('True or False:', True or False)
+print(not 3 > 2)     # False - because 3 > 2 is true, then not True gives False
+print(not True)      # False - Negation, the not operator turns true to false
+print(not False)     # True
+print(not not True)  # True
+print(not not False) # False
+
+# 1. Declare your age as integer variable
+my_age = 30
+# 2. Declare your height as a float variable
+my_height = 1.71 # meters
+# 3. Declare a variable that store a complex number
+complex_number = 1j
+print("compelx number 1j =", complex_number)
+# 4. Write a script that prompts the user to enter base and height of the triangle and calculate an area of this triangle (area = 0.5 x b x h).
+base = 4
+height = 6
+area = base * height * 0.5
+
+print("area triangle = ", area)
