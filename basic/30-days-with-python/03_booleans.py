8267da5b71ca5336ec216f95d3d427ad81fff65f
Marco Rossel <andresmarcorossel@gmail.com>
2023-08-06 01:15:13 -0400

    python | backend | 03

--- basic/30-days-with-python/03_booleans.py
+++ basic/30-days-with-python/03_booleans.py
@@ -1,2 +1,17 @@
 # Day 3 ---> Boolean
 
+# print(1 is 1)               # true
+# print(1 is not 2)           # true
+# print('A' in "Asabeneh")    # true
+
+""" Logical Operators
+Unlike other programming languages python uses keywords and, or and not for logical operators. Logical operators are used to combine conditional statements: """
+
+# and -- return true if both statements are true
+print('operador and : 1 < 5 and 2 > 1 = ', 1 < 5 and 2 > 1) # true
+
+# or -- return true if one of the statements is true
+print('operador or : 1 < 5 or 1  > 10 = ', 1 < 5 or 1 > 10) # true
+
+# not -- reverse the result, return false if the result is ture
+print('operador not :  not(2 < 5) = ', not(2 < 5)) # false
