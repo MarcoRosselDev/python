5e81ddbd9c67b63c40ecabf53ab64738ee624227
Marco Rossel <andresmarcorossel@gmail.com>
2023-08-09 18:20:05 -0400

    python | 30 days | 02 exercises level 1

---
+++ basic/30-days-with-python/02_functions/02_level_1.py
@@ -0,0 +1,15 @@
+### Day 2: 30 Days of python programming
+
+first_name = 'Marco'
+last_name = 'Rossel'
+full_name = first_name + ' ' + last_name
+country = 'Chileno'
+city = 'Concepcion'
+age = 30
+year = 2023
+is_married = False
+is_true = True
+is_light_on = False
+
+### Declare multiple variable on one line
+a, b, c, d = 1, 4, 6, 2 
