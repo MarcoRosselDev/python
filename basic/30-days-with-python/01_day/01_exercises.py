dc20e932fd375fc40ecda4878b80db8f19fdc704
Marco Rossel <andresmarcorossel@gmail.com>
2023-08-09 13:31:40 -0400

    python | 30 days | 01 exercises A

---
+++ basic/30-days-with-python/01_day/01_exercises.py
@@ -0,0 +1,38 @@
+""" 1. check the pyton versio you are using
+
+in terminal ---> python --version
+output --------> Python 3.11.4
+"""
+
+""" 2. Open the pyton interactive shell and do the following operations.
+
+The operands are 3 and 4.
+    - addition(+)
+    - subtraction(-)
+    - multiplication(\*)
+    - modulus(%)
+    - division(/)
+    - exponential(\*\*)
+    - floor division operator(//)
+"""
+
+x = 3
+y = 4
+print('addition : ', x + y)
+print('subtraction : ', x - y)
+print('multiplication : ', x * y)
+print('modulus : ', x % y)
+print('division : ', x / y)
+print('exponential : ', x ** y)
+print('floor division operator : ', x // y)
+""" output:
+
+addition :  7
+subtraction :  -1
+multiplication :  12
+modulus :  3
+division :  0.75
+exponential :  81
+floor division operator :  0
+
+"""
