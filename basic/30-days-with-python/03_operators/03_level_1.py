1cea32a741532804d02ff98dbe2611bb26076d5d
Marco Rossel <andresmarcorossel@gmail.com>
2023-08-11 12:56:26 -0400

    python | 30 days | 03 exercises level 1

---
+++ basic/30-days-with-python/03_operators/03_level_1.py
@@ -0,0 +1,10 @@
+## 1. Declare your age as integer variable
+my_age = 30
+
+## 2. Declare your height as a float variable
+my_hight = 1.72 # meters
+
+## 3. Declare a variable that store a complex number
+my_complex_number = 4j
+
+## 4. Write a script that prompts the user to enter base and height of the triangle and calculate an area of this triangle (area = 0.5 x b x h).
