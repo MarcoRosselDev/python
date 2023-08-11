f08e95c2230cb3cd9cd206d5593c2e6a0de8940a
Marco Rossel <andresmarcorossel@gmail.com>
2023-08-11 17:02:13 -0400

    python | 30 days | 03 exercises level 1 D

--- basic/30-days-with-python/03_operators/03_lever_2.py
+++ basic/30-days-with-python/03_operators/03_lever_2.py
@@ -1,12 +1,19 @@
 ## 8. Calculate the slope, x-intercept and y-intercept of y = 2x -2
-
 x = 0
 y = 2*x - 2
-
-print('La interseccion en el eje x es: ', y) # -2
-# por lo tanto la interseccion en x = -2
-
+print('Interseccion en eje y cuando x es cero es : ', y) ## -2
 y = 0
-x = (y / 2) + 2
+x = (y/2) + 1
+print('Interseccion en eje x cuando y es cero es: ', x) ## 1
 
-print('La interseccion en el eje y es :', x) # 2
+## 9. Slope is (m = y2-y1/x2-x1). Find the slope and [Euclidean distance] between point (2, 2) and point (6,10)
+
+x1 = int(input('Enter the x1 value please : '))
+y1 = int(input('Enter the y1 value please : '))
+x2 = int(input('Enter the x2 value please : '))
+y2 = int(input('Enter the y2 value please : '))
+
+pendiente = (y2 - y1)/(x2 - x1)
+euclidean_distance = 1/2*((x2 - x1)**2 + (y2 - y1)**2)
+print('La pendiente de estos dos puntos son : ', pendiente)
+print('La distancia euclideana entre estos dos punto es : ', euclidean_distance)
