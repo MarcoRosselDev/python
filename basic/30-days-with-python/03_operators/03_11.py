043b8a3eb6c0015f1350369542e16375e005a26b
Marco Rossel <andresmarcorossel@gmail.com>
2023-08-11 18:40:00 -0400

    python | 30 days | 03 exercises level 11

---
+++ basic/30-days-with-python/03_operators/03_11.py
@@ -0,0 +1,19 @@
+## 11. Calculate the value of y (y = x^2 + 6x + 9). Try to use different x values and figure out at what x value y is going to be 0.-------------------
+x = -7
+y = (x**2) + 6*x + 9
+print('value of y when x = 0 : ', y)
+
+""" 
+x = -7  ----> y = 16
+x = -6  ----> y = 9
+x = -5  ----> y = 4
+x = -4  ----> y = 1
+x = -3  ----> y = 0 -----> -3 y = 0
+x = -2  ----> y = 1
+x = -1  ----> y = 4
+x = 0   ----> y = 9
+x = 1   ----> y = 16
+x = 2   ----> y = 25
+x = 3   ----> y = 36
+x = 4   ----> y = 49
+"""
--- basic/30-days-with-python/03_operators/03_lever_2.py
+++ basic/30-days-with-python/03_operators/03_lever_2.py
@@ -13,7 +13,11 @@ y1 = int(input('Enter the y1 value please : '))
 x2 = int(input('Enter the x2 value please : '))
 y2 = int(input('Enter the y2 value please : '))
 
-pendiente = (y2 - y1)/(x2 - x1)
-euclidean_distance = 1/2*((x2 - x1)**2 + (y2 - y1)**2)
+pendiente = (y2 - y1)/(x2 - x1) # 2
+euclidean_distance = ((x2 - x1)**2 + (y2 - y1)**2)**(1/2) # 8.9442719099
 print('La pendiente de estos dos puntos son : ', pendiente)
 print('La distancia euclideana entre estos dos punto es : ', euclidean_distance)
+
+## 10. Compare the slopes in tasks 8 and 9.---------------------------------
+# la pendiente de el ejercicio 8 es 2
+# la pendiente del ejecicio 9 es 2  ---> tienen la misma pendiente
