fea74f6f473eeef3dece538294900f238adb88f4
Marco Rossel <andresmarcorossel@gmail.com>
2023-08-11 22:20:58 -0400

    python | 30 days | 03 exercises  14 - 18

---
+++ basic/30-days-with-python/03_operators/03_17.py
@@ -0,0 +1,7 @@
+## 17. Even numbers are divisible by 2 and the remainder is zero. How do you check if a number is even or not using python?
+
+x = 3
+
+is_par = x % 2 == 0
+print(is_par)
+## we are checking if the reminder is 0, if is, then is par.
---
+++ basic/30-days-with-python/03_operators/03_18.py
@@ -0,0 +1,5 @@
+## 18. Check if the floor division of 7 by 3 is equal to the int converted value of 2.7.
+
+division = (7 / 3) == 2.7
+print(division) ## False
+## actually is 2.3333333
