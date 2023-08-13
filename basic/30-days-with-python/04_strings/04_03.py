c665b7f8ae070c5fff952744a1a756a74e2623fc
Marco Rossel <andresmarcorossel@gmail.com>
2023-08-12 20:51:00 -0400

    python | 30 days | 04 strings exercise 2 - 5

---
+++ basic/30-days-with-python/04_strings/04_03.py
@@ -0,0 +1,10 @@
+### 3. Declare a variable named company and assign it to an initial value "Coding For All".
+company = "Coding For All"
+
+### 4. Print the variable company using _print()_.
+print('var company : ',company)
+
+### 5. Print the length of the company string using _len()_ method and _print()_.
+print('Lenght of the string company : ',len(company))
+
+### 6. Change all the characters to uppercase letters using _upper()_ method.
