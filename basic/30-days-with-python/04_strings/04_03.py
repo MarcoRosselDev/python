00d5c6adb240d10384f71c19f37cc138e335b20c
Marco Rossel <andresmarcorossel@gmail.com>
2023-08-12 21:00:49 -0400

    python | 30 days | 04 strings exercise 5 - 8

--- basic/30-days-with-python/04_strings/04_03.py
+++ basic/30-days-with-python/04_strings/04_03.py
@@ -8,3 +8,18 @@ print('var company : ',company)
 print('Lenght of the string company : ',len(company))
 
 ### 6. Change all the characters to uppercase letters using _upper()_ method.
+result = company.upper()
+print(result) # CODING FOR ALL
+
+### 7. Change all the characters to lowercase letters using lower() method.
+result = company.lower()
+print(result) # coding for all
+
+### 8. Use capitalize(), title(), swapcase() methods to format the value of the string Coding For All.
+
+capitalize = company.capitalize()
+title = company.title()
+swapcase = company.swapcase()
+print('capitalize : ', capitalize)  # capitalize :  Coding for all
+print('title : ', title)            # title :  Coding For All
+print('swapcase : ', swapcase)      # swapcase :  cODING fOR aLL
