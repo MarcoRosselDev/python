8467c6855a2f40affc098953326b9aaec7bd06e0
Marco Rossel <andresmarcorossel@gmail.com>
2023-08-13 16:45:31 -0400

    python | 30 days | 04 strings exercise 11

---
+++ basic/30-days-with-python/04_strings/04_11.py
@@ -0,0 +1,7 @@
+### 11. Replace the word coding in the string 'Coding For All' to Python.
+
+company = 'Coding For All'
+
+result = company.replace('Coding', 'Python')
+print(result)
+## Python For All
