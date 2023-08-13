7cf7d44827e79d7408af656f5c148a5ea7f1f839
Marco Rossel <andresmarcorossel@gmail.com>
2023-08-12 20:41:49 -0400

    python | 30 days | 04 strings exercise 1 - 2

---
+++ basic/30-days-with-python/04_strings/04_01.py
@@ -0,0 +1,6 @@
+### 1. Concatenate the string 'Thirty', 'Days', 'Of', 'Python' to a single string, 'Thirty Days Of Python'.
+
+frase = ['Thirty', 'Days', 'Of', 'Python']
+
+result = ' '.join(frase)
+print(result) # Thirty Days Of Python
---
+++ basic/30-days-with-python/04_strings/04_02.py
@@ -0,0 +1,5 @@
+### 2. Concatenate the string 'Coding', 'For' , 'All' to a single string, 'Coding For All'.
+
+code = ['Coding', 'For' , 'All']
+result = ' '.join(code)
+print(result) ## Coding For All
