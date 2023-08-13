a89137babd5c83a8cc0f195fbe3793d5a8149664
Marco Rossel <andresmarcorossel@gmail.com>
2023-08-12 21:15:25 -0400

    python | 30 days | 04 strings exercise 8 - 10

---
+++ basic/30-days-with-python/04_strings/04_09.py
@@ -0,0 +1,6 @@
+### Cut(slice) out the first word of Coding For All string.
+
+company = 'Coding For All'
+
+result_slice = company[0:6]
+print(result_slice) # Coding
---
+++ basic/30-days-with-python/04_strings/04_10.py
@@ -0,0 +1,6 @@
+### 10. Check if Coding For All string contains a word Coding using the method index, find or other methods.
+
+company = 'Coding For All'
+coding = 'Coding'
+result = company.index(coding)
+print(result) # 0
