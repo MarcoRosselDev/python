503246c2742fa7983e329c969179f9c52539a1fb
Marco Rossel <andresmarcorossel@gmail.com>
2023-08-13 20:50:47 -0400

    python | 30 days | 04 strings exercise 18 - 21

---
+++ basic/30-days-with-python/04_strings/04_18.py
@@ -0,0 +1,15 @@
+### 18. Create an acronym or an abbreviation for the name 'Python For Everyone'.
+pfe = 'Python For Everyone'
+
+### 19. Create an acronym or an abbreviation for the name 'Coding For All'.
+cfa = 'Coding For All'
+
+### 20. Use index to determine the position of the first occurrence of C in Coding For All.
+first_position_c = cfa.index('C')
+print(first_position_c) # 0
+
+### 21. Use index to determine the position of the first occurrence of F in Coding For All.
+first_position_f = cfa.index('F')
+print(first_position_f) # 7
+
+### 22. Use rfind to determine the position of the last occurrence of l in Coding For All People.
