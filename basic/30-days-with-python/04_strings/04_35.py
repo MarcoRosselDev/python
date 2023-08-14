73777ac6cdbe00807052f972a250230e790be7ed
Marco Rossel <andresmarcorossel@gmail.com>
2023-08-14 19:59:04 -0400

    python | 30 days | 04 strings exercise 35 - 36

---
+++ basic/30-days-with-python/04_strings/04_35.py
@@ -0,0 +1,36 @@
+### 35. Use the string formatting method to display the following: 
+#The area of a circle with radius 10 is 314 meters square.
+radius = 10
+area = int(3.14 * radius ** 2)
+print(f'The area of a circle with raius {radius} is {area} meters square.')
+# The area of a circle with raius 10 is 314 meters square.
+
+### 36. Make the following using string formatting methods:
+""" 
+8 + 6 = 14
+8 - 6 = 2
+8 * 6 = 48
+8 / 6 = 1.33
+8 % 6 = 2
+8 // 6 = 1
+8 ** 6 = 262144 """
+
+a = 8
+b = 6
+
+print('{} + {} = {}'.format(a, b, a + b))
+print('{} - {} = {}'.format(a, b, a - b))
+print('{} * {} = {}'.format(a, b, a * b))
+print('{} / {} = {:.2f}'.format(a, b, a / b))
+print('{} % {} = {}'.format(a, b, a % b))
+print('{} // {} = {}'.format(a, b, a // b))
+print('{} ** {} = {}'.format(a, b, a ** b))
+
+""" 
+8 + 6 = 14
+8 - 6 = 2
+8 * 6 = 48
+8 / 6 = 1.33
+8 % 6 = 2
+8 // 6 = 1
+8 ** 6 = 262144 """
