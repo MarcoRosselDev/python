506e05497e687716b25f2b2f183d24bcee97908e
Marco Rossel <andresmarcorossel@gmail.com>
2023-08-13 23:13:36 -0400

    python | 30 days | 04 strings exercise 23 - 25

--- basic/30-days-with-python/04_strings/04_23.py
+++ basic/30-days-with-python/04_strings/04_23.py
@@ -3,3 +3,16 @@
 challenge = 'You cannot end a sentence with because because because is a conjunction'
 solution = challenge.find('because')
 print(solution) # 31
+
+### 24. Use rindex to find the position of the last occurrence of the word because in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
+
+last_because = challenge.rfind('because')
+last_because_index = last_because + len('because ')
+print('position last because : ', last_because_index)
+# position last because :  55
+
+### 25. Slice out the phrase 'because because because' in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
+
+slice_challenge = challenge[0:31] + challenge[55:]
+print(slice_challenge)
+# You cannot end a sentence with is a conjunction
