80625f4f289d1f6d32b2ce69fe0210622aafe9ca
Marco Rossel <andresmarcorossel@gmail.com>
2023-08-13 20:39:34 -0400

    python | 30 days | 04 strings exercise 13 - 14

---
+++ basic/30-days-with-python/04_strings/04_13.py
@@ -0,0 +1,6 @@
+### 13. Split the string 'Coding For All' using space as the separator (split()) .
+
+challenge = 'Coding For All'
+print(challenge.split( )) # ['Coding', 'For', 'All']
+challenge = 'thirty, days, of, python'
+print(challenge.split(', ')) # ['thirty', 'days', 'of', 'python']
---
+++ basic/30-days-with-python/04_strings/04_14.py
@@ -0,0 +1,8 @@
+### 14. "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon" split the string at the comma.
+
+challenge = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
+
+solution = challenge.split(',')
+print(solution)
+## output =
+## ['Facebook', ' Google', ' Microsoft', ' Apple', ' IBM', ' Oracle', ' Amazon']
