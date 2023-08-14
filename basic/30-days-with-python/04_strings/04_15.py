cef35081d34a78d8278e15ed0c55dc6bae84e36e
Marco Rossel <andresmarcorossel@gmail.com>
2023-08-13 20:45:40 -0400

    python | 30 days | 04 strings exercise 15 - 17

---
+++ basic/30-days-with-python/04_strings/04_15.py
@@ -0,0 +1,9 @@
+### 15. What is the character at index 0 in the string Coding For All.
+challenge = 'Coding For All'
+print(challenge[0]) # C
+
+### 16. What is the last index of the string Coding For All.
+print(challenge[-1]) # l
+
+### 17. What character is at index 10 in "Coding For All" string.
+print(challenge[10]) # " " ---> es un espacio!
