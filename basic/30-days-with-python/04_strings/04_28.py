467fb9773c5fea9b0bcb7028292d9f242bf3ba75
Marco Rossel <andresmarcorossel@gmail.com>
2023-08-14 12:57:18 -0400

    python | 30 days | 04 strings exercise 27 - 28

---
+++ basic/30-days-with-python/04_strings/04_28.py
@@ -0,0 +1,10 @@
+### 28. Does ''Coding For All' start with a substring Coding?
+challenge = 'Coding For All'
+does_start = challenge.startswith('Coding')
+print('Does challenge start with a substring "Coding" ? : ', does_start)
+# Does challenge start with a substring "Coding" ? :  True
+
+### 29. Does 'Coding For All' end with a substring _coding_?
+does_end = challenge.endswith('Coding')
+print('Does challenge var end with "Coding" ? : ', does_end)
+# Does challenge var end with "Coding" ? :  False
