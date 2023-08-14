ee694ec0b2cd775eb0829d2f496fd2e403cedd36
Marco Rossel <andresmarcorossel@gmail.com>
2023-08-13 20:11:36 -0400

    python | 30 days | 04 strings exercise 12

---
+++ basic/30-days-with-python/04_strings/04_12.py
@@ -0,0 +1,8 @@
+### 12. Change Python for Everyone to Python for All using the replace method or other methods.
+frase = 'Python for Everyone'
+print(frase)
+result = frase.replace('Everyone', 'All')
+print('.replace : ', result)
+## output:
+## Python for Everyone
+##.replace :  Python for All
