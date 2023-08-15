f78f453f65d2c8daba320cc413e658271db87062
Marco Rossel <andresmarcorossel@gmail.com>
2023-08-15 16:29:52 -0400

    python | 30 days | 05 list 21 - 24

--- basic/30-days-with-python/05_list/05_21.py
+++ basic/30-days-with-python/05_list/05_21.py
@@ -1,2 +1,20 @@
 ### 21. Remove the first IT company from the list
 it_companies = ['Oracle', 'Microsoft', 'IBM', 'Google', 'Facebook', 'Apple', 'Amazon']
+it_companies.remove('Oracle')
+print(it_companies)
+# ['Microsoft', 'IBM', 'Google', 'Facebook', 'Apple', 'Amazon']
+
+### 22. Remove the middle IT company or companies from the list
+# print(len(it_companies)) # 6
+del it_companies[2:4] # [0,1,2,3,4,5] <--- 2 = comienza incluyendolo, 4 = termina no incluyendolo
+print('before del : ',it_companies)
+# ['Microsoft', 'IBM', 'Apple', 'Amazon']
+
+### 23. Remove the last IT company from the list
+del it_companies[-1:]
+print('delete last item : ',it_companies)
+# delete last item :  ['Microsoft', 'IBM', 'Apple']
+
+### 24. Remove all IT companies from the list
+del it_companies[:]
+print(it_companies) # []
---
+++ basic/30-days-with-python/05_list/05_pop.py
@@ -0,0 +1,7 @@
+### built in function .pop
+
+it_companies = ['Facebook', 'Google', 'Amazon', 'IBM']
+## it_companies.pop(0, 1) ----> no se puede pasar mas de un argumento
+it_companies.pop(0)
+print(it_companies)
+# ['Google', 'Amazon', 'IBM']
