bf9f9cb16c5c7ab481c8d0d8a8c9466e5db18972
Marco Rossel <andresmarcorossel@gmail.com>
2023-08-15 02:06:38 -0400

    python | 30 days | 05 list 17 - 20

---
+++ basic/30-days-with-python/05_list/05_18.py
@@ -0,0 +1,13 @@
+### 18. Slice out the first 3 companies from the list
+it_companies = ['Oracle', 'Microsoft', 'IBM', 'Google', 'Facebook', 'Apple', 'Amazon']
+slice_first_tree = it_companies[3:]
+print(slice_first_tree)
+# ['Google', 'Facebook', 'Apple', 'Amazon']
+
+### 19. Slice out the last 3 companies from the list
+print(len(it_companies)) # 7
+slice_last_tree = it_companies[:4]
+print(slice_last_tree)
+# ['Oracle', 'Microsoft', 'IBM', 'Google']
+
+### 20. Slice out the middle IT company or companies from the list
