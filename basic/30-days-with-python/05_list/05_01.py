bab87a46ed3abf3292245531c05830520daa4178
Marco Rossel <andresmarcorossel@gmail.com>
2023-08-14 20:42:34 -0400

    python | 30 days | 05 list 1 - 3

---
+++ basic/30-days-with-python/05_list/05_01.py
@@ -0,0 +1,13 @@
+### 1. Declare an empty list
+
+my_list = list()
+print(my_list)
+# []
+
+### 2. Declare a list with more than 5 items
+my_list = ['a', 'b', 'c', 'd', 12]
+
+### 3. Find the length of your list
+print(len(my_list)) # 5
+
+### 4. Get the first item, the middle item and the last item of the list
