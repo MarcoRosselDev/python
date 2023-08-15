770b56dc8825660d10507cad8203b6efe1026efc
Marco Rossel <andresmarcorossel@gmail.com>
2023-08-15 01:42:28 -0400

    python | 30 days | 05 list 14 - 15

---
+++ basic/30-days-with-python/05_list/05_14.py
@@ -0,0 +1,11 @@
+it_companies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+
+### 14. Join the it_companies with a string '#;  '
+list_join = "#; ".join(it_companies)
+print(list_join)
+# FACEBOOK#; Google#; Microsoft#; Apple#; Github#; IBM#; Oracle#; Mercado Libre#; Amazon
+
+### 15. 
+does_exist = 'IBM' in it_companies
+print('IBM is in IT list? : ',does_exist)
+# IBM is in IT list? :  True
