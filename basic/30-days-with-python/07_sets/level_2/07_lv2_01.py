42977de409259dff3ea3a0353da3e6ed507cd590
Marco Rossel <andresmarcorossel@gmail.com>
2023-08-16 23:10:37 -0400

    python | 30 days | 07 sets | exer lv2 5 - 7

--- basic/30-days-with-python/07_sets/level_2/07_lv2_01.py
+++ basic/30-days-with-python/07_sets/level_2/07_lv2_01.py
@@ -21,3 +21,20 @@ print(is_ssubset)                       # True
 ### 4. Are A and B disjoint sets
 is_disjoin = A.isdisjoint(B)
 print(is_disjoin)                       # False
+
+### 5. Join A with B and B with A
+join_a_with_b = A.union(B)
+print('join A with B : ', join_a_with_b)# join A with B :  {19, 20, 22, 24, 25, 26, 27, 28}
+
+join_b_with_a = B.union(A)
+print('join A with B : ', join_b_with_a)# join A with B :  {19, 20, 22, 24, 25, 26, 27, 28}
+
+### 6. What is the symmetric difference between A and B
+symmetric_difference = A.symmetric_difference(B)
+print(symmetric_difference)             # {27, 28}
+
+### 7. Delete the sets completely
+del symmetric_difference
+del join_a_with_b
+del join_b_with_a
+del is_disjoin                          # del ...
