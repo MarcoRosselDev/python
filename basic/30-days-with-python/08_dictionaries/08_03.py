07de83be03f2c0fa90dfa3088c0ae0f5b2c41655
Marco Rossel <andresmarcorossel@gmail.com>
2023-08-17 03:07:35 -0400

    python | 30 days | 08 dictionaries | 5 -11

--- basic/30-days-with-python/08_dictionaries/08_03.py
+++ basic/30-days-with-python/08_dictionaries/08_03.py
@@ -1,9 +1,10 @@
-### 3. Create a student dictionary and add first_name, last_name, gender, age, marital status, skills, country, city and address as keys for the dictionary
+### 3. Create a student dictionary and add first_name, last_name, gender, 
+### age, marital status, skills, country, city and address as keys for the dictionary------------
 
 student = dict()
 student['first_name'] = 'Marco'
 student['last_name'] = 'Rossel'
-student['gender'] = False
+student['gender'] = 'Male'
 student['age'] = 30
 student['marital'] = False
 student['skills'] = ['JavaScript', 'React', 'Python', 'NodeJS']
@@ -14,7 +15,50 @@ student['address'] = {
     'n_street' : 'nueva los copihues'
 }
 
-### 4. Get the length of the student dictionary
+### 4. Get the length of the student dictionary----------------------------------------------------
 print(len(student))         # 9
 
-### 5. Get the value of skills and check the data type, it should be a list
+### 5. Get the value of skills and check the data type, it should be a list------------------------
+
+print('value of skills : ', student['skills'])
+# value of skills :  ['JavaScript', 'React', 'Python', 'NodeJS']
+
+print('data type of skills : ', type(student['skills']))
+# data type of skills :  <class 'list'>
+
+### 6. Modify the skills values by adding one or two skills----------------------------------------
+student['skills'] = student['skills'] + ['Git', 'TypeScript']
+print('students skills after edition : ', student['skills'])
+# students skills after edition :  ['JavaScript', 'React', 'Python', 'NodeJS', 'Git', 
+# 'TypeScript']
+
+### 7. Get the dictionary keys as a list-----------------------------------------------------------
+print(student.keys())
+# dict_keys(['first_name', 'last_name', 'gender', 'age', 'marital', 'skills', 'country', 
+# 'city', 'address'])
+
+### 8. Get the dictionary values as a list---------------------------------------------------------
+print(student.values())
+# dict_values(['Marco', 'Rossel', 'Male', 30, False, ['JavaScript', 'React', 'Python', 
+# 'NodeJS', 'Git', 'TypeScript'], 'Chilean', 'Concepcion', 
+# {'n_house': 522, 'n_street': 'nueva los copihues'}])
+
+### 9. Change the dictionary to a list of tuples using items() method------------------------------
+change_tuples = student.items()
+print(change_tuples)
+# dict_items([('first_name', 'Marco'), ('last_name', 'Rossel'), ('gender', 'Male'), 
+# ('age', 30), ('marital', False), ('skills', ['JavaScript', 'React', 'Python', 'NodeJS', 'Git', 
+# 'TypeScript']), ('country', 'Chilean'), ('city', 'Concepcion'), ('address', 
+# {'n_house': 522, 'n_street': 'nueva los copihues'})])
+
+### 10. Delete one of the items in the dictionary--------------------------------------------------
+del student['address']
+del student['skills']
+
+print(student)
+# {'first_name': 'Marco', 'last_name': 'Rossel', 'gender': 'Male', 'age': 30, 'marital': False, 
+# 'country': 'Chilean', 'city': 'Concepcion'}
+
+### 11. Delete one of the dictionaries
+del student
+print(student)  # None
