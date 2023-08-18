97c4587da6f8a65184eafc306b52b46ec393e6b4
Marco Rossel <andresmarcorossel@gmail.com>
2023-08-18 17:45:43 -0400

    python | 30 days | 10 loops | 01

---
+++ basic/30-days-with-python/10_loops/10_01.py
@@ -0,0 +1,25 @@
+### 1. Iterate 0 to 10 using for loop, do the same using while loop.
+
+print('this is using for loop')
+for a in list(range(0, 11)):
+    print(a)
+""" output
+this is using for loop
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+"""
+
+print('this is using while loop')
+loop = 0
+while loop < 11:
+    print(loop)
+    loop = loop + 1
