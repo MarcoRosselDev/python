caf4c9b15f66cd8b7146cc93ebffee4f8f0d08d2
Marco Rossel <andresmarcorossel@gmail.com>
2023-08-18 20:24:27 -0400

    python | 30 days | 10 loops | 05

---
+++ basic/30-days-with-python/10_loops/10_05.py
@@ -0,0 +1,50 @@
+### 5. Print the following pattern:
+
+""" 
+0 x 0 = 0
+1 x 1 = 1
+2 x 2 = 4
+3 x 3 = 9
+4 x 4 = 16
+5 x 5 = 25
+6 x 6 = 36
+7 x 7 = 49
+8 x 8 = 64
+9 x 9 = 81
+10 x 10 = 100 """
+
+print('this is with for loop')
+for num in range(0, 11):
+    print(f'{num} x {num} = {num*num}')
+
+print('this is with while loop')
+numero = 0
+while numero < 11:
+    print(f'{numero} x {numero} = {numero*numero}')
+    numero = numero + 1
+
+""" output
+this is with for loop
+0 x 0 = 0
+1 x 1 = 1
+2 x 2 = 4
+3 x 3 = 9
+4 x 4 = 16
+5 x 5 = 25
+6 x 6 = 36
+7 x 7 = 49
+8 x 8 = 64
+9 x 9 = 81
+10 x 10 = 100
+this is with while loop
+0 x 0 = 0
+1 x 1 = 1
+2 x 2 = 4
+3 x 3 = 9
+4 x 4 = 16
+5 x 5 = 25
+6 x 6 = 36
+7 x 7 = 49
+8 x 8 = 64
+9 x 9 = 81
+10 x 10 = 100 """
