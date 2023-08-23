d7d536a1f4ce7d4e3133ce4c65d2b37e1bd9ad14
Marco Rossel <andresmarcorossel@gmail.com>
2023-08-22 23:57:51 -0400

    python | 30 days | 12 modules | import 01 - first steep

--- basic/30-days-with-python/12_modules/02_test/12_01.py
+++ basic/30-days-with-python/12_modules/02_test/12_01.py
@@ -5,10 +5,24 @@ print(random_user_id());
 '1ee33d' """
 
 from random import random, randint
-from string import string
 
-str_letters = string.ascii_letters
-str_digits = string.digits
+# para no llamar el modulo string mejor tomo las letras y numeros de un string
+str_letters = 'abcdefghijklmnopqrstuvwxyz'
+str_letters_mayusc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+str_digits = '0123456789'
+
+def list_creator(string):
+    list_returned = []
+    for letter in string:
+        list_returned.append(letter)
+    return list_returned
+
+# creamos listas de letras mayusculas, minusculas y numeros para llamarlos de forma aleatoria
+letters_list = list_creator(str_letters)
+numbers_list = list_creator(str_digits)
+letters_list_mayusc = list_creator(str_letters_mayusc)
+
+
 
 def random_user_id():
     print('break')
