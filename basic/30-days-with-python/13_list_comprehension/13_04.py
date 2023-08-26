f87f5892c926a59152b7a9a2659ff453d38941ed
Marco Rossel <andresmarcorossel@gmail.com>
2023-08-26 11:44:01 -0400

    python | 30 days | 13 list comprehension | 4 - successful

--- basic/30-days-with-python/13_list_comprehension/13_04.py
+++ basic/30-days-with-python/13_list_comprehension/13_04.py
@@ -5,6 +5,17 @@ output:
 
 countries = [[('Finland', 'Helsinki')], [('Sweden', 'Stockholm')], [('Norway', 'Oslo')]]
 
-a = [box for box in countries for num in box  for box  in num ]
+#a = [box for box in countries for num in box  for box  in num ]
+f = []
+for arr in countries:
+    for tuple in arr:
+        tuple = list(tuple)
+        tuple.insert(1, tuple[0][:3])
+        for item in range(len(tuple)):
+            tuple[item] = tuple[item].upper()
+        f.append(tuple)
 
-print(a)
+print(f)
+
+""" output:
+[['FINLAND', 'FIN', 'HELSINKI'], ['SWEDEN', 'SWE', 'STOCKHOLM'], ['NORWAY', 'NOR', 'OSLO']] """
