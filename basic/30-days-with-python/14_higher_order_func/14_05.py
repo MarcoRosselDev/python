c92e8687ff8af87b9fcc80b3176ca42ccc465d60
Marco Rossel <andresmarcorossel@gmail.com>
2023-08-26 14:29:04 -0400

    python | 30 days | 14 high order functions | 5 - 6

---
+++ basic/30-days-with-python/14_higher_order_func/14_05.py
@@ -0,0 +1,12 @@
+""" 5. Use for to print each name in the names list. """
+
+names = ['Asabeneh', 'Lidiya', 'Ermias', 'Abraham']
+
+for each in names:
+    print(each)
+
+""" output:
+Asabeneh
+Lidiya
+Ermias
+Abraham """
---
+++ basic/30-days-with-python/14_higher_order_func/14_06.py
@@ -0,0 +1,18 @@
+""" 6. Use for to print each number in the numbers list. """
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for each in numbers:
+    print(each)
+
+""" output
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10 """
