5bbfcde708ee24a5973043d0b07b07fcc84b6629
Marco Rossel <andresmarcorossel@gmail.com>
2023-08-28 16:38:33 -0400

    python | 30 days | 14 high order functions | lvl 3 | 1

---
+++ basic/30-days-with-python/14_higher_order_func/level_3/14_lvl3_1.py
@@ -0,0 +1,7 @@
+""" 1. Use the countries_data.py 
+(https://github.com/Asabeneh/30-Days-Of-Python/blob/master/data/countries-data.py) 
+file and follow the tasks below: """
+# Sort countries by name, by capital, by population
+
+from countries_data import dict_countries
+print(dict_countries[0])
